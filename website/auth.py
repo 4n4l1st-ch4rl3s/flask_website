d5ea6a7c8c178bccffd92ef8016fdc9d896bb568
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-04-02 23:24:25 +0300

    more changes

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 auth = Blueprint('auth', __name__)
@@ -28,6 +30,10 @@ def sign_up():
             flash("Password must be at least 7 characters long", category="error")
         else:
             #add user to database
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha1'), password2=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("You have successfully created a new user", category="success")
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
